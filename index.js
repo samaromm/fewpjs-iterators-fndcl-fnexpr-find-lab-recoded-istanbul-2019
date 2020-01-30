0e484c93bf51cacb237f0f83b78b9f9ffb25ea77
Samar Omar <samaromm98@gmail.com>
2020-01-30 10:41:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function testFunc() {
 
 
 function superbowlWin(arr){
-  let win = arr.find(function(e){return e[result]=='w'})
-  if(win)return win.year
+  let result = arr.find(function(e){return e[result]=='w'})
+  if(result)return result.year
 }
