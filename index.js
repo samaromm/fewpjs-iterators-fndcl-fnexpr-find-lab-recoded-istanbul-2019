34ff310a69e467eac99091a9c1914d45a79519a9
Samar Omar <samaromm98@gmail.com>
2020-01-30 10:28:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function testFunc() {
   return "hi"
 }
 
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+
 function superbowlWin(arr){
   
 }
